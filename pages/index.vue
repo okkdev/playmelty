2a7558a45cadf44c279e5a46f2651542722fd787
jen.stehlik <jen.stehlik@kallysoft.ch>
2020-01-08 14:57:38 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mx-auto h-full">
+    <div class="flex flex-col h-full justify-center items-center">
+      <h1 class="text-6xl">
+        meltyclub
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+#__nuxt {
+  background-color: rgba(0, 0, 0, 0.6);
+}
+
+body {
+  background: url('/img/wallpaper.jpg');
+  @apply bg-cover bg-center bg-scroll text-white;
+}
+</style>

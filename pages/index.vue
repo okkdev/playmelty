55c5a47604a1eb335995f3d850f561cf6cbc0237
jen.stehlik <jen.stehlik@kallysoft.ch>
2020-01-10 17:07:33 +0100

    Modals impemented

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,23 @@ export default {
     } else {
       window.location.replace('https://play.meltyblood.club')
     }
+  },
+  head() {
+    return {
+      title: 'Play Melty Blood!',
+      meta: [
+        { name: 'og:title', content: 'Play Melty Blood you wimp!' },
+        {
+          name: 'og:description',
+          content: 'With the power of friendship and poverty.'
+        },
+        {
+          name: 'og:image',
+          content: 'https://meltyblood.club/img/sway.gif'
+        },
+        { name: 'og:image', content: 'https://play.meltyblood.club' }
+      ]
+    }
   }
 }
 </script>
